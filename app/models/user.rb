03109526f17d889bd9b0a4f5428510d126483476
farzam <l144211@lhr.nu.edu.pk>
2017-10-07 19:54:09 +0500

    Active admin gem is configured with project and user model is added as resource in it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  def active_for_authentication? 
+    super && approved? 
+  end 
+  
+  def inactive_message 
+    if !approved? 
+      :not_approved 
+    else 
+      super "Successfully Approved."
+    end 
+  end
+  
+  def self.send_reset_password_instructions(attributes={})
+    recoverable = find_or_initialize_with_errors(reset_password_keys, attributes, :not_found)
+    if !recoverable.approved?
+      recoverable.errors[:base] << I18n.t("devise.failure.not_approved")
+    elsif recoverable.persisted?
+      recoverable.send_reset_password_instructions
+    end
+    recoverable
+  end
 end
